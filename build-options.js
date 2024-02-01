6a60ff6a3012fa0d617fa684606d2c470e73f690
Mike Landi <miclanm@amazon.com>
2024-02-01 12:02:35 -0500

    update readme

--- build-options.js
+++ build-options.js
@@ -1,9 +1,16 @@
 #!/bin/node
 
+const version = '0.3.64';
+
 const encoded = encodeURIComponent(
   JSON.stringify({
     repository: 'https://github.com/aws-solutions/quota-monitor-for-aws.git',
+    deployment: {
+      cloudFormationTemplate: 'deployment/quota-monitor-hub.template',
+    },
   })
 );
 
-console.log(encoded);
+console.log(
+  `[![Launch](https://img.shields.io/badge/Launch%20with%20CodeCatalyst-%F0%9F%9A%80-8A2BE2)](https://integ.stage.quokka.codes/spaces/blueprints/blueprints/%40amazon-codecatalyst%2Fblueprints.launch-blueprint/publishers/ae796a3f-a165-4125-aefe-2d2d494de859/versions/${version}/projects/create?options=${encoded})`
+);
